c09f77629c0fdc2fe9933d90bc0a8cb3249eb648
2003thanhquy <quythanhhuynh2003@gmail.com>
2024-04-27 12:11:32 +0700

    add login form

--- src/main/java/org/LapTrinhTienTien/repository/TaiKhoanRepository.java
+++ src/main/java/org/LapTrinhTienTien/repository/TaiKhoanRepository.java
@@ -4,4 +4,5 @@ import org.LapTrinhTienTien.model.TaiKhoan;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TaiKhoanRepository extends CrudRepository<TaiKhoan, Long> {
+    TaiKhoan findByTaiKhoan(String taiKhoan);
 }
